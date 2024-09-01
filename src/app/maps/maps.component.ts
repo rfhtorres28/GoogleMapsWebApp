b240271613f253567702bb9295029adaf3539a34
rfhtorres28 <rfhtorres28@gmail.com>
2024-09-01 21:13:44 +0800

    UpdateGoogleMapApp

--- src/app/maps/maps.component.ts
+++ src/app/maps/maps.component.ts
@@ -2,18 +2,21 @@ import { Component } from '@angular/core';
 import { GoogleMap } from '@angular/google-maps';
 import { GoogleMapsModule } from '@angular/google-maps';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-maps',
   standalone: true,
-  imports: [GoogleMap, GoogleMapsModule, CommonModule],
+  imports: [GoogleMap, GoogleMapsModule, CommonModule, FormsModule],
   templateUrl: './maps.component.html',
   styleUrl: './maps.component.css'
 })
 export class MapsComponent {
    latitude: number = 0; 
    longitude: number = 0;
+   inputLatitude: string = "";
+   inputLongitude: string = "";
    position = {lat: 70.73061, lng: -73.935242};
    markers = [
     {
@@ -29,18 +32,28 @@ export class MapsComponent {
       label: 'A'
     }  
   ]
+  center: google.maps.LatLngLiteral = { lat: 14.279451427593996, lng: 121.1005105 }; // sets the center of the map
+  centerTest: google.maps.LatLngLiteral = { lat: 0, lng: 0 };
+  options: google.maps.MapOptions = { maxZoom: 20, minZoom: 3};
   info: {position: {lat:number, lng:number}, label: string} = {position: {lat: 0, lng:0}, label: ""}
+  testCoordinates: {position: {lat:number, lng:number}, label: string} = {position: {lat: 0, lng:0}, label: ""}
 
-   location(event: google.maps.MapMouseEvent) {
+  location(event: google.maps.MapMouseEvent) {
    
       this.latitude = event.latLng?.toJSON().lat ?? 0;
       this.longitude = event.latLng?.toJSON().lng ?? 0;
       console.log(`${this.latitude }, ${this.longitude }`);
 
    this.info = {position: {lat: this.latitude, lng:this.longitude}, label: "A"};
-    
-   }
 
+   }
+  
+   
+   onCoordinatesChange() {
+      const latValue = parseFloat(this.inputLatitude);
+      const longValue = parseFloat(this.inputLongitude);
+      this.centerTest = {lat: latValue, lng:longValue}
+   }
 
 
 
